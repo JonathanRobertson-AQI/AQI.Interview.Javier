0f6c50339c03247275407f65e8756c884e5afd76
Jonathan Robertson <jonathan.robertson@aquaticinformatics.com>
2022-12-07 16:15:18 -0700

    scaffolding the solution

---
+++ src/AQI.Interview.Models/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AQI.Interview.Models
+{
+    public class Measurement
+    {
+        public long MeasurementId { get; set; }
+        public double? NumericValue { get; set; }
+        public string StringValue { get; set; }
+        public int Precision { get; set; }
+        public Parameter Parameter { get; set; }
+        public DateTime UTCCapturedTimestamp { get; set; }
+        public DateTime UTCSavedTimestamp { get; set; }
+    }
+}
---
+++ src/AQI.Interview.Models/Parameter.cs
@@ -0,0 +1,12 @@
+﻿namespace AQI.Interview.Models
+{
+    public enum Parameter
+    {
+        AirTemperature = 1,
+        Alkalinity = 2,
+        Calcium = 3,
+        Chlorine = 4,
+        Flow = 5,
+        Phosphorus = 6
+    }
+}
